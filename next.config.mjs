eba1e7d47021498f9d77e1a74cba30ae194222e7
Mehtab Singh <mehtab.singh@edu.sait.ca>
2024-11-13 12:29:38 -0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Your Next.js configuration options
+    output: 'export',
+    reactStrictMode: true,
+    swcMinify: true,
+    // Add other configurations as needed
+  };
+  
+  export default nextConfig;
