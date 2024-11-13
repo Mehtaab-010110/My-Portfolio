66b1a729d2bf2deff5d775b138c7e10ac182f887
Mehtab Singh <mehtab.singh@edu.sait.ca>
2024-11-13 15:15:32 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
